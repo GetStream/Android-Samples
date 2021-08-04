2e1d9949ad509e9fda2256dc191ccfbdf9a68554
Adit lal <aditlal90@gmail.com>
2021-08-04 12:13:25 +0530

    [2126] Adds a test user for testing live stream channels

--- livestream-compose-sample/src/main/java/io/getstream/livestream/compose/App.kt
+++ livestream-compose-sample/src/main/java/io/getstream/livestream/compose/App.kt
@@ -19,16 +19,16 @@ class App : Application() {
         ChatDomain.Builder(client, applicationContext).build()
 
         val user = User(
-            id = "1f37e58d-d8b0-476a-a4f2-f8611e0d85d9",
+            id = "aditlal",
             extraData = mutableMapOf(
-                "name" to "Jc",
+                "name" to "Adit Lal",
                 "image" to "https://firebasestorage.googleapis.com/v0/b/stream-chat-internal.appspot.com/o/users%2FJc.png?alt=media",
             ),
         )
 
         client.connectUser(
             user = user,
-            token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoiMWYzN2U1OGQtZDhiMC00NzZhLWE0ZjItZjg2MTFlMGQ4NWQ5In0.l3u9P1NKhJ91rI1tzOcABGh045Kj69-iVkC2yUtohVw"
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiYWRpdGxhbCJ9.Q8oM9czJlsGV5RiV2pEF-y9XnwUh--5XbF1j7Dc861U"
         ).enqueue()
     }
 
