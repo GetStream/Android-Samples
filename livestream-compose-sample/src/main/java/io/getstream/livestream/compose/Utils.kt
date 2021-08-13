0c52c1fb21e0355fbeda2491c439d2b85f172591
Adit lal <aditlal90@gmail.com>
2021-08-13 15:48:52 +0530

    [2126] Updates comments design

--- livestream-compose-sample/src/main/java/io/getstream/livestream/compose/Utils.kt
+++ livestream-compose-sample/src/main/java/io/getstream/livestream/compose/Utils.kt
@@ -3,6 +3,8 @@ package io.getstream.livestream.compose
 import android.content.Context
 import android.content.res.TypedArray
 import java.util.Random
+import java.util.Calendar
+import java.util.Date
 
 fun Context.randomArtWork(): Int {
     val rand = Random()
@@ -19,5 +21,44 @@ fun Context.randomDescription(): Int {
     val rndInt: Int = rand.nextInt(descArray.length())
     return descArray.getResourceId(rndInt, 0).also {
         descArray.recycle()
+    }
+}
+
+private const val SECOND_MILLIS = 1000
+private const val MINUTE_MILLIS = 60 * SECOND_MILLIS
+private const val HOUR_MILLIS = 60 * MINUTE_MILLIS
+private const val DAY_MILLIS = 24 * HOUR_MILLIS
+
+fun Context.getTimeAgo(date: Date): String {
+    val now: Date = Calendar.getInstance().time
+    val diff: Long = now.time - date.time
+    return when {
+        diff < SECOND_MILLIS -> {
+            getString(R.string.just_now)
+        }
+        diff < MINUTE_MILLIS -> {
+            String.format(getString(R.string.seconds_ago), diff / SECOND_MILLIS)
+        }
+        diff < 2 * MINUTE_MILLIS -> {
+            String.format(getString(R.string.minutes_ago), 1)
+        }
+        diff < 59 * MINUTE_MILLIS -> {
+            String.format(getString(R.string.minutes_ago), diff / MINUTE_MILLIS)
+        }
+        diff < 90 * MINUTE_MILLIS -> {
+            String.format(getString(R.string.hour_ago), diff / HOUR_MILLIS)
+        }
+        diff < 48 * HOUR_MILLIS -> {
+            getString(R.string.yesterday)
+        }
+        diff < 6 * DAY_MILLIS -> {
+            String.format(getString(R.string.hour_ago), diff / DAY_MILLIS)
+        }
+        diff <8* DAY_MILLIS ->{
+            getString(R.string.week_ago)
+        }
+        else -> {
+            getString(R.string.long_time)
+        }
     }
 }
--- livestream-compose-sample/src/main/java/io/getstream/livestream/compose/ui/LiveStreamComment.kt
+++ livestream-compose-sample/src/main/java/io/getstream/livestream/compose/ui/LiveStreamComment.kt
@@ -2,18 +2,23 @@ package io.getstream.livestream.compose.ui
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import io.getstream.chat.android.client.models.name
 import io.getstream.chat.android.compose.state.messages.items.MessageItem
+import io.getstream.chat.android.compose.ui.common.MessageBubble
 import io.getstream.chat.android.compose.ui.common.avatar.UserAvatar
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.livestream.compose.getTimeAgo
 import java.text.SimpleDateFormat
 import java.util.Date
 
@@ -28,10 +33,11 @@ fun LiveStreamComment(
     modifier: Modifier = Modifier,
     messageItem: MessageItem
 ) {
+    val context = LocalContext.current
     Row(
         modifier = modifier
             .padding(start = 8.dp, bottom = 2.dp)
-            .widthIn(max = 300.dp),
+            .width(300.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         UserAvatar(
@@ -50,19 +56,23 @@ fun LiveStreamComment(
                 )
                 Text(
                     modifier = Modifier.padding(start = 4.dp),
-                    text = SimpleDateFormat.getTimeInstance()
-                        .format(messageItem.message.createdAt ?: Date()),
+                    text = context.getTimeAgo(messageItem.message.createdAt ?: Date()),
                     style = ChatTheme.typography.footnote,
                     color = ChatTheme.colors.textLowEmphasis
                 )
             }
+            MessageBubble(
+                color = ChatTheme.colors.overlay,
+                shape = ChatTheme.shapes.otherMessageBubble
+            ) {
+                Text(
+                    modifier = Modifier.padding(8.dp),
+                    text = messageItem.message.text,
+                    style = ChatTheme.typography.body,
+                    color = ChatTheme.colors.textHighEmphasis
+                )
+            }
 
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = messageItem.message.text,
-                style = ChatTheme.typography.body,
-                color = ChatTheme.colors.textHighEmphasis
-            )
         }
     }
 }
