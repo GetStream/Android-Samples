beb13604e8ca80059ebb27ed69fe6a44d1b37209
Adit lal <aditlal90@gmail.com>
2021-08-17 19:14:25 +0530

    [2127] Added skeleton project structure for slack compose app

---
+++ slack-compose-sample/src/main/java/io/getstream/compose/slack/presentation/HomeActivity.kt
@@ -0,0 +1,15 @@
+package io.getstream.compose.slack.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class HomeActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
