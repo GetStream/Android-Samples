49a39a47f95c7c275b3399aedabd5234b3b4ada2
Adit lal <aditlal90@gmail.com>
2021-08-18 14:01:44 +0530

    [2127] Added a default chat sdk init code

---
+++ slack-compose-sample/src/main/java/io/getstream/compose/slack/App.kt
@@ -0,0 +1,36 @@
+package io.getstream.compose.slack
+
+import android.app.Application
+import androidx.camera.camera2.Camera2Config
+import androidx.camera.core.CameraXConfig
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.livedata.ChatDomain
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
+
+        val client = ChatClient.Builder("kumvbr5ah5jg", applicationContext)
+            .build()
+        ChatDomain.Builder(client, applicationContext).build()
+
+        val user = User(
+            id = "filip-b",
+            extraData = mutableMapOf(
+                "name" to "Filip B",
+                "image" to "https://firebasestorage.googleapis.com/v0/b/stream-chat-internal.appspot.com/o/users%2FJc.png?alt=media",
+            ),
+        )
+
+        client.connectUser(
+            user = user,
+            token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiZmlsaXAtYiJ9.MrBdSmiRFB1CRzWFXprjcXPo86W4H2N82KRgJVCXt60"
+        ).enqueue()
+    }
+}
