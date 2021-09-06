00840256a923c95bb7ec03b247cffe248218e17b
Samuel Urbanowicz <samiuelson@gmail.com>
2021-09-06 09:44:01 +0200

    Format code

--- virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/overview/OverviewFragment.kt
+++ virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/overview/OverviewFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
