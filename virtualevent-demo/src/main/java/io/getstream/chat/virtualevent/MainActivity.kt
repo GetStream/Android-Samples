eada723688e43486e047ed24ee811a63dd68fc52
Samuel Urbanowicz <samiuelson@gmail.com>
2021-09-09 11:36:47 +0200

    Simplify MainActivity

--- virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/MainActivity.kt
+++ virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/MainActivity.kt
@@ -1,12 +1,5 @@
 package io.getstream.chat.virtualevent
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
-}
+class MainActivity : AppCompatActivity(R.layout.activity_main)
