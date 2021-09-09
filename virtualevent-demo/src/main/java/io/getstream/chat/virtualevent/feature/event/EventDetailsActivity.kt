65d81dcc9e6f57aa3c0fab05178d420106f6e664
Samuel Urbanowicz <samiuelson@gmail.com>
2021-09-09 10:37:46 +0200

    Add back button to event details screen

--- virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/event/EventDetailsActivity.kt
+++ virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/event/EventDetailsActivity.kt
@@ -1,7 +1,14 @@
 package io.getstream.chat.virtualevent.feature.event
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import io.getstream.chat.virtualevent.R
 
 class EventDetailsActivity : AppCompatActivity(R.layout.activity_event_details) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+        }
+    }
 }
