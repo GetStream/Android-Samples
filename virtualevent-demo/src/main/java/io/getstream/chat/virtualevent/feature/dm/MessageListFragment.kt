f4caa2ee59f0fa9e6d6464f8c3dd6876a492e66f
Dmitrii Bychkov <mail@dbychkov.com>
2021-09-09 03:01:24 +0300

    [CAS-1220] Fix lint errors

--- virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/dm/MessageListFragment.kt
+++ virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/feature/dm/MessageListFragment.kt
@@ -1,11 +1,10 @@
 package io.getstream.chat.virtualevent.feature.dm
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.getstream.sdk.chat.viewmodel.MessageInputViewModel
