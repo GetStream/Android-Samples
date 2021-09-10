a165a0f59b0a44dac28d9101331527216cae424c
Dmitrii Bychkov <mail@dbychkov.com>
2021-09-10 09:45:50 +0300

    [CAS-1220] Disable toolbar in theme

--- virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/MainActivity.kt
+++ virtualevent-demo/src/main/java/io/getstream/chat/virtualevent/MainActivity.kt
@@ -3,10 +3,10 @@ package io.getstream.chat.virtualevent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.virtualevent.databinding.ActivityMainBinding
+import java.lang.IllegalArgumentException
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,11 +17,33 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navController = findNavController(R.id.navHostFragment)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.navigation_overview, R.id.navigation_events, R.id.navigation_dm)
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        binding.bottomNavigationView.setupWithNavController(navController)
+        setupToolbar()
+        setupBottomNavigation()
+    }
+
+    private fun setupToolbar() {
+        ChatClient.instance().getCurrentUser()?.let {
+            binding.userAvatarView.setUserData(it)
+        }
+    }
+
+    private fun setupBottomNavigation() {
+        with(binding.bottomNavigationView) {
+            // disable reloading fragment when clicking again on the same tab
+            setOnNavigationItemReselectedListener {}
+
+            val navController = findNavController(R.id.navHostFragment)
+            setupWithNavController(navController)
+            navController.addOnDestinationChangedListener { _, destination, _ ->
+                val titleRes = when (destination.id) {
+                    R.id.navigation_overview -> R.string.title_overview
+                    R.id.navigation_events -> R.string.title_events
+                    R.id.navigation_dm -> R.string.title_dm
+                    else -> throw IllegalArgumentException("Unsupported navigation item")
+                }
+
+                binding.toolbarTitleTextView.text = getString(titleRes)
+            }
+        }
     }
 }
