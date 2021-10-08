9c24b5a738f128f01700553773ccc4081165afdc
Dmitrii Bychkov <mail@dbychkov.com>
2021-10-08 20:14:29 +0300

    [2127] Add ripple effect

--- slack-clone-compose-sample/src/main/kotlin/io/getstream/slack/compose/ui/channels/ChannelItem.kt
+++ slack-clone-compose-sample/src/main/kotlin/io/getstream/slack/compose/ui/channels/ChannelItem.kt
@@ -2,6 +2,7 @@ package io.getstream.slack.compose.ui.channels
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -15,7 +16,9 @@ import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -61,7 +64,11 @@ fun ChannelItem(
         Row(
             modifier = Modifier
                 .clip(shape = RoundedCornerShape(6.dp))
-                .clickable(onClick = { onChannelClick(channel) })
+                .clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = rememberRipple(),
+                    onClick = { onChannelClick(channel) }
+                )
                 .padding(horizontal = 8.dp)
                 .fillMaxSize(),
             verticalAlignment = Alignment.CenterVertically,
