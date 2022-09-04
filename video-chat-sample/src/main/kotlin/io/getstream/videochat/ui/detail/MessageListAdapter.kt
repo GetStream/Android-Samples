e8d61fa7402516a16212569c9bdc7b4f57e839f8
skydoves <skydoves@naver.com>
2022-09-05 08:19:41 +0900

    Refactor video-chat project

--- video-chat-sample/src/main/kotlin/io/getstream/videochat/ui/detail/MessageListAdapter.kt
+++ video-chat-sample/src/main/kotlin/io/getstream/videochat/ui/detail/MessageListAdapter.kt
@@ -25,41 +25,41 @@
 package io.getstream.videochat.ui.detail
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import coil.transform.CircleCropTransformation
 import io.getstream.chat.android.client.models.Message
-import io.getstream.videochat.R
+import io.getstream.videochat.databinding.ItemMessageBinding
 
 class MessageListAdapter :
     ListAdapter<Message, MessageListAdapter.MessageViewHolder>(MessageDiffCallback) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
-        return LayoutInflater
-            .from(parent.context).inflate(R.layout.item_message, parent, false)
-            .let { MessageViewHolder(it) }
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        MessageViewHolder(
+            ItemMessageBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
 
     override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class MessageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val avatarImageView: ImageView by lazy { itemView.findViewById(R.id.avatarImageView) }
-        private val userNameTextView: TextView by lazy { itemView.findViewById(R.id.nameTextView) }
-        private val messageTextView: TextView by lazy { itemView.findViewById(R.id.messageTextView) }
+    class MessageViewHolder(private val binding: ItemMessageBinding) :
+        RecyclerView.ViewHolder(binding.root) {
 
         fun bind(message: Message) {
-            userNameTextView.text = message.user.name
-            messageTextView.text = message.text
-            avatarImageView.load(message.user.image) {
-                transformations(CircleCropTransformation())
+            with(binding) {
+                nameTextView.text = message.user.name
+                messageTextView.text = message.text
+                avatarImageView.load(message.user.image) {
+                    transformations(CircleCropTransformation())
+                }
             }
         }
     }
